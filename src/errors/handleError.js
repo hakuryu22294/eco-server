03f38d31fbb09f5c6388c47524b934ce229bd696
hakuryu22294 <92999763+hakuryu22294@users.noreply.github.com>
2024-04-11 14:34:16 +0700

    c

--- src/errors/handleError.js
+++ src/errors/handleError.js
@@ -18,7 +18,7 @@ const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
   const message = `Invalid input data. ${errors.join(". ")}`;
-  return new AppError(message, 400);
+  return new GlobalError(message, 400);
 };
 
 const handleTokenExpiredError = () => {
